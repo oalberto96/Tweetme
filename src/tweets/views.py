c470c86ad214599f3ad5e921d601d8c69798fe9d
Alberto <oalberto96c@gmail.com>
2018-01-25 16:17:01 -0800

    Class based views

--- src/tweets/views.py
+++ src/tweets/views.py
@@ -1,21 +1,18 @@
 
 from django.shortcuts import render
-
+from django.views.generic import DetailView, ListView
 from .models import Tweet
 
 # Create your views here.
-
-def tweet_detail_view(request, id=1):
-	obj = Tweet.objects.get(id=id)
-	print(obj)
-	context = {
-		"object": obj
-	}
-	return render(request, "tweets/detail_view.html", context)
-
-def tweet_list_view(request):
+class TweetDetailView(DetailView):
+	template_name = "tweets/detail_view.html"
 	queryset = Tweet.objects.all()
-	context = {
-		"object_list": queryset
-	}
-	return render(request, "tweets/list_view.html", context)
+
+	def get_object(self):
+		return Tweet.objects.get(id=1)
+
+class TweetListView(ListView):
+	template_name = "tweets/list_view.html"
+	queryset = Tweet.objects.all()
+
+
